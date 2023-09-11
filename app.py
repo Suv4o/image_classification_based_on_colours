a386843115e0591aa152fd970bc9838ba927d0c2
Aleks <aleksandar.trpkovski@gmail.com>
2023-09-11 21:58:01 +1000

    chore: add .example.env file to provide an example of the required environment variables feat: add langchain and python-dotenv packages to Pipfile to support language processing and environment variable loading fix(app.py): import dotenv and load_dotenv to load environment variables from .env file feat(app.py): add OPENAI_API_KEY environment variable to store the OpenAI API key feat(app.py): create langchain ChatOpenAI instance with model "gpt-4", temperature 0, and OpenAI API key from environment variable feat(app.py): create ChatPromptTemplate for the conversation with system and human messages feat(app.py): define json_schema for the structured output chain feat(app.py): create structured output chain using json_schema, langchain ChatOpenAI instance, and ChatPromptTemplate feat(app.py): iterate through hex_colours and use the structured output chain to classify each hex colour into a colour name

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
 import os
 
+from langchain.chains.openai_functions import (
+    create_openai_fn_chain,
+    create_structured_output_chain,
+)
+from langchain.chat_models import ChatOpenAI
+from langchain.prompts import ChatPromptTemplate, HumanMessagePromptTemplate
+from langchain.schema import HumanMessage, SystemMessage
 from sklearn.cluster import KMeans
 from PIL import Image
+from dotenv import load_dotenv
 from collections import Counter
 import numpy as np
 import cv2
+
+load_dotenv()
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 
 
 def get_image(pil_image):
@@ -36,8 +47,35 @@ def get_colours(pimg):
 
 
 images = os.listdir("images")
+llm = ChatOpenAI(model="gpt-4", temperature=0, openai_api_key=OPENAI_API_KEY)
+prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            "You are a hex colour to colour name converter. You are given a hex colour and you must return the colour name. The hex colour that must belong in one of the following descriptive colour labels: Red, Crimson, Scarlet, Vermilion, Maroon, Rose, Pink, Magenta, Fuchsia, Purple, Lavender, Indigo, Blue, Navy, Azure, Cyan, Teal, Turquoise, Green, Emerald, Lime, Chartreuse, Olive, Yellow, Gold, Amber, Orange, Peach, Apricot, Brown, Sienna, Chocolate, Tan, Beige, Khaki, Gray, Silver, Charcoal, White, Ivory, Cream, Pearl, Platinum, Jet Black, Onyx Black",
+        ),
+        ("human", "Use the given hex color to classify into a colour name: {input}"),
+        ("human", "Tip: Make sure to use the labels that were provided to classify the colour."),
+    ]
+)
+
+json_schema = {
+    "title": "Colour",
+    "description": "Convert a hex colour to a colour name",
+    "type": "object",
+    "properties": {
+        "colour_name": {"type": "string", "description": "The colour name"},
+        "hex_colour": {"type": "string", "description": "The hex colour"},
+    },
+    "required": ["colour_name"],
+}
+
+
+chain = create_structured_output_chain(json_schema, llm, prompt)
 
 for image in images:
     pil_image = Image.open("images/" + image)
     hex_colours = get_colours(pil_image)
-    print(hex_colours)
+    for hex in hex_colours:
+        colour_name = chain.run(f"Hex Colour: {hex}")
+        print(colour_name)
